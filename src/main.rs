e92a8145abe43fe7680d7e3f2f24431ead14b992
Mikael Lux <mikael.lux@singular-it.de>
2022-11-03 19:36:53 +0100

    secant method

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 mod solvers;
-use crate::solvers::solvers::newtons_method;
 
 fn main() {
-    let root = newtons_method(&f, &f_prime, 0.5, 0.00001, 10000);
-    println!("{}", root.unwrap());
+    let root1 = solvers::newtons_method(&f, &f_prime, 0.5, 0.00001, 10000);
+    println!("{}", root1.unwrap());
+    let root2 = solvers::secant_method(&f, 0.0, 1.0, 0.00001, 10000);
+    println!("{}", root2.unwrap());
 }
 
-
 fn f(x: f64) -> f64 {
-    x+x.exp()-(2 as f64)
+    x + x.exp() - (2 as f64)
 }
 
 fn f_prime(x: f64) -> f64 {
-    x.exp()+(1 as f64)
+    x.exp() + (1 as f64)
 }
