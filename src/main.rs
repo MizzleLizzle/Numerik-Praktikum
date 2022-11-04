7e918e44b9e258f6c86d85ad8cc802c3aa3103b1
intervention128 <intervention128@gmail.com>
2022-11-04 16:49:39 +0100

    add basic plotting function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod solvers;
+use plotters::prelude::*;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    build_plot(&f_1)?;
     let series = solvers::newtons_method_error_series(&f_1, &f_1_prime, 1.0, 0.00001, 10000);
     println!("{:?}", series.unwrap());
     let series2 = solvers::secant_method_error_series(&f_1, 0.0, 1.0, 0.00001, 10000);
@@ -9,6 +11,40 @@ fn main() {
     println!("{}", root3.unwrap());
     let root4 = solvers::fixed_point_method(&g_1_b, 0.1, 0.00001, 10000);
     println!("{}", root4.unwrap());
+    Ok(())
+}
+
+fn build_plot(
+    f: &dyn Fn(f64) -> f64,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+    let root = BitMapBackend::new("{name}.png", (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&root)
+        .caption("name", ("sans-serif", 50).into_font())
+        .margin(5)
+        .x_label_area_size(30)
+        .y_label_area_size(30)
+        .build_cartesian_2d(-2f64..2f64, -2f64..10f64)?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(LineSeries::new(
+            (-50..=50).map(|x| x as f64 / 50.0).map(|x| (x, f(x))),
+            &RED,
+        ))?
+        .label("y = x^2")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+    
+    chart
+        .configure_series_labels()
+        .background_style(&WHITE.mix(0.8))
+        .border_style(&BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    Ok(())
 }
 
 fn f_1(x: f64) -> f64 {
